87e723b491838e0898a78a5be82db1bd5d1a06d2
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2022-11-11 10:56:39 -0700

    feat: Add generate_random method and tests

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,6 +1,12 @@
 class Enigma
-  attr_reader :alphabet
+  attr_reader :alphabet, :random_number
+
   def initialize
     @alphabet = ("a".."z").to_a << " "
+    @random_number = generate_random
+  end
+
+  def generate_random
+    rand(99999).to_s.rjust(5, "0").to_i
   end
 end
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -3,11 +3,24 @@ require './lib/enigma'
 RSpec.describe Enigma do
   let(:enigma) { Enigma.new }
 
-  it 'exists' do
-    expect(enigma).to be_a(Enigma)
+  describe "#initialize" do
+    it 'exists' do
+      expect(enigma).to be_a(Enigma)
+    end
+
+    it 'generates an array of letters plus space' do
+      expect(enigma.alphabet).to eq(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "])
+    end
   end
 
-  it 'generates an array of letters plus space' do
-    expect(enigma.alphabet).to eq(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "])
+  describe "#generate_random" do
+    it 'generates a random five digit number' do
+      first_number = enigma.generate_random
+      second_number = enigma.generate_random
+
+      expect(first_number).to_not eq(second_number)
+      expect(enigma.random_number.digits.size).to eq(5)
+      expect(enigma.random_number).to be_a(Integer)
+    end
   end
 end
