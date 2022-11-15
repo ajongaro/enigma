22728597a754ffbb7b137a8a4bec5ef4fa7bdb63
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2022-11-14 18:38:21 -0700

    fix: move generators to module

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -24,30 +24,13 @@ class Enigma
     generate_shift
   end
 
-  def determine_proper(shift)
+  def shift_for(shift)
     case shift
     when 1 then return @shift[:A]
     when 2 then return @shift[:B]
     when 3 then return @shift[:C]
     when 4 then return @shift[:D]
     end
-  end
-
-  def decrypt(message, key, date)
-    generator(key, date)
-    shift = 0
-    output = []
-
-    message.chars.each do |letter|
-      shift += 1
-      letter_position = @reverse_alphabet.find_index(letter) 
-
-      new_alphabet = @reverse_alphabet.rotate(determine_proper(shift))
-      output << new_alphabet[letter_position]
-      shift = 0 if shift == 4
-    end
-
-    { decryption: output.join, key: key, date: date }
   end
 
   def encrypt(message, key=@random_number, date=GET_DATE)
@@ -59,36 +42,35 @@ class Enigma
       shift += 1
       letter_position = @alphabet.find_index(letter) 
 
-      output << @alphabet.rotate(determine_proper(shift))[letter_position]
+      output << @alphabet.rotate(shift_for(shift))[letter_position]
       shift = 0 if shift == 4
     end
 
     { encryption: output.join, key: key, date: date }
   end
 
-  def generate_shift
-    @shift[:A] = @key[:A] + @offset[:A]
-    @shift[:B] = @key[:B] + @offset[:B]
-    @shift[:C] = @key[:C] + @offset[:C]
-    @shift[:D] = @key[:D] + @offset[:D]
-  end
+  def decrypt(message, key, date)
+    generator(key, date)
+    shift = 0
+    output = []
 
-  def generate_keys_from(hash)
-    @key[:A] = hash.split("")[0..1].join.to_i
-    @key[:B] = hash.split("")[1..2].join.to_i
-    @key[:C] = hash.split("")[2..3].join.to_i
-    @key[:D] = hash.split("")[3..4].join.to_i
+    message.chars.each do |letter|
+      shift += 1
+      letter_position = @reverse_alphabet.find_index(letter) 
+
+      output << @reverse_alphabet.rotate(shift_for(shift))[letter_position]
+      shift = 0 if shift == 4
+    end
+
+    { decryption: output.join, key: key, date: date }
   end
 
   def date_to_offset(date)
     (date.to_i**2).to_s[-4..-1].split("").map(&:to_i)
   end
 
-  def generate_offsets(date=GET_DATE)
-    @offset[:A], @offset[:B], @offset[:C], @offset[:D] = date_to_offset(date)
-  end
-
-  # def fetch_date
-  #   Date.today.strftime('%d%m%y')
+  # def generate_offsets(date=GET_DATE)
+  #   @offset[:A], @offset[:B], @offset[:C], @offset[:D] = date_to_offset(date)
   # end
+
 end
